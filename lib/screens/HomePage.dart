d11ffc979517f4880388c6ac8d8e79a7e832c7fb
mhasib <mhasib@vmware.com>
2019-12-20 02:27:05 +0530

    X)

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -24,10 +24,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future fetchToken() async {
     var url =
-        'https://www.reddit.com/api/v1/access_token?grant_type=refresh_token&refresh_token=10667572874-C_BHgHe98oTlUc4O4qKZoyCYFSY';
-    Map<String, String> headers = {
-      'Authorization': 'Basic M0g2RzY2Qm1ZRmFfOHc6'
-    };
+        'https://www.reddit.com/api/v1/access_token?grant_type=refresh_token&refresh_token=xxxxxxxxxxx-C_BHgHe98oTlUc4O4qKZoyCYFSY';
+    Map<String, String> headers = {'Authorization': 'Basic xxxxxxxxxxx'};
     var response = await http.post(url, headers: headers);
     return response;
   }
